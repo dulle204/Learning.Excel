a323d4d63fa7ec5c2fb74dd86f5c1edfdef609a3
d.ivanisevic <ivanisevic_dusan@yahoo.com>
2016-04-23 18:32:19 +0200

    first commit

---
+++ Learning.Excel/ExcelDocHandler.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Learning.Excel
+{
+    public class ExcelDocHandler
+    {
+        public List<Person> GetAll()
+        {
+
+        }
+    }
+}
---
+++ Learning.Excel/Person.cs
@@ -0,0 +1,6 @@
+﻿namespace Learning.Excel
+{
+    internal class Person
+    {
+    }
+}
