f97b4aa2c659ed2da1cc3830d199bae2a98786a1
d.ivanisevic <ivanisevic_dusan@yahoo.com>
2016-04-23 18:46:37 +0200

    person

--- Learning.Excel/Person.cs
+++ Learning.Excel/Person.cs
@@ -1,6 +1,23 @@
 ﻿namespace Learning.Excel
 {
-    internal class Person
+    public class Person
     {
+        public int ID { get; set; }
+
+        public string FirstName { get; set; }
+
+        public string LastName { get; set; }
+
+        public string Email { get; set; }
+
+        public Gender Genfeder { get; set; }
     }
 }
+
+
+
+    public enum Gender
+    {
+        Male, 
+        Female
+    }
